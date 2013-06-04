2e6e728c19d5753230fefe88a3adc08e668d2be3
Oleg Eterevsky <oleg@eterevsky.com>
2013-06-04 18:18:15 +0400

    Select, delete & add profile UI.

--- js/background.js
+++ js/background.js
@@ -2,10 +2,62 @@ function log(s) {
   console.log(s);
 }
 
+var profiles = [1, 2, 3];
+var profileData = {
+  1: {'name': 'Default'},
+  2: {'name': 'Old'},
+  3: {'name': 'Test Profile'}
+};
+
 function getProfiles() {
-  return [{'id': 1, 'name': 'Default'},
-          {'id': 2, 'name': 'Old'},
-          {'id': 3, 'name': 'Test'}];
+  var profilesWithNames = [];
+  for (var i = 0; i < profiles.length; i++) {
+    var id = profiles[i];
+    var name = profileData[id]['name'];
+    profilesWithNames.push({'id': id, 'name': name});
+  }
+  return profilesWithNames;
+}
+
+function getProfileName(id) {
+  return profileData[id]['name'];
+}
+
+function getLastUsedProfile() {
+  return 1;
+}
+
+function deleteProfile(id) {
+  profiles.splice(profiles.indexOf(id), 1);
+  delete profileData[id];
+}
+
+function newProfile(id, name) {
+  return {'id': id, 'name': name};
+}
+
+function addProfile() {
+  var id = profiles[profiles.length - 1] + 1;
+  var name;
+
+  var i;
+  for (i = 1;; i++) {
+    var found = false;
+    name = 'Profile ' + i;
+    for (var j = 0; j < profiles.length; j++) {
+      if (profileData[profiles[j]]['name'] === name) {
+        found = true;
+        break;
+      }
+    }
+    if (!found)
+      break;
+  }
+
+  var profile = newProfile(id, name);
+  profiles.push(id);
+  profileData[id] = profile;
+  return id;
 }
 
 var MOCK_PASSWORDS = {1: 'abc', 2: '12345678'};
--- js/options.js
+++ js/options.js
@@ -1,7 +1,92 @@
 var background = null;
+var selectedProfileId = null;
 
 function init() {
-  chrome.runtime.getBackgroundPage(function(bg) {background = bg;});
+  chrome.runtime.getBackgroundPage(onBackgroundPage);
 }
+
+function onBackgroundPage(bg) {
+  background = bg;
+  document.getElementById('add-profile').addEventListener('click', addProfile);
+  populateProfiles(background.getProfiles());
+  selectProfile(background.getLastUsedProfile());
+}
+
+function populateProfiles(profiles) {
+  var profilesRow = document.getElementById('profile-titles');
+  var addProfileButton = document.getElementById('add-profile');
+
+  var toDelete = [];
+  for (var i = 0; i < profilesRow.children.length; i++) {
+    var child = profilesRow.children[i];
+    if (child.tagName.toLowerCase() === 'span')
+      toDelete.push(child);
+  }
+
+  for (var i = 0; i < toDelete.length; i++) {
+    profilesRow.removeChild(toDelete[i]);
+  }
+
+  for (var i = 0; i < profiles.length; i++) {
+    var profileId = profiles[i]['id'];
+    var profileName = profiles[i]['name'];
+
+    var span = document.createElement('span');
+    span.id = 'profile-' + profileId;
+
+    var title = document.createElement('a');
+    title.appendChild(document.createTextNode(profileName));
+    title.addEventListener('click', selectProfile.bind(null, profileId));
+    span.appendChild(title);
+
+    var del = document.createElement('a');
+    del.appendChild(document.createTextNode('\u00d7'));
+    del.classList.add('delete-profile');
+    del.addEventListener('click', deleteProfile.bind(null, profileId));
+    span.appendChild(del);
+
+    profilesRow.insertBefore(span, addProfileButton);
+  }
+}
+
+/**
+ * @param {number} profileId
+ */
+function selectProfile(profileId) {
+  if (selectedProfileId) {
+    var oldProfile = document.getElementById('profile-' + selectedProfileId);
+    if (oldProfile)
+      oldProfile.classList.remove('selected-profile');
+  }
+
+  selectedProfileId = profileId;
+  document.getElementById('profile-' + profileId).classList
+      .add('selected-profile');
+}
+
+/**
+ * @param {number} profileId
+ */
+function deleteProfile(profileId) {
+  var name = background.getProfileName(profileId);
+
+  if (confirm('Delete the profile "' + name + '"?')) {
+    background.deleteProfile(profileId);
+    populateProfiles(background.getProfiles());
+    if (profileId === selectedProfileId) {
+      selectProfile(background.getLastUsedProfile());
+    } else {
+      selectProfile(selectedProfileId);
+    }
+  }
+}
+
+
+function addProfile() {
+  var profileId = background.addProfile();
+  populateProfiles(background.getProfiles());
+  selectProfile(profileId);
+}
+
 
 window.addEventListener('load', init);
