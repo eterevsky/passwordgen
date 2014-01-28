7e59c9b8db5f0828776abf64a05a85a0020db1ef
Oleg Eterevsky <oleg@eterevsky.com>
2014-01-29 01:25:07 +0400

    Remove unneeded stuff in build.

--- build.py
+++ build.py
@@ -2,12 +2,9 @@ import os
 
 import builder
 import builder.chrome_app as chrome_app
-import builder.html as html
 import builder.util as util
 
 FILES = ['main.css', 'options.css']
-
-HTML_FILES = ['popup.html', 'options.html']
 
 @builder.action
 def build(source_dir, base_build_path=None):
@@ -15,7 +12,7 @@ def build(source_dir, base_build_path=None):
   build_path = util.build_path_with_version(source_dir, base_build_path,
                                             short_name)
 
-  app_builder = builder.chrome_app.Builder(source_dir, build_path)
+  app_builder = chrome_app.Builder(source_dir, build_path)
   app_builder.add_static_files(FILES)
   app_builder.add_html_file('popup.html')
   app_builder.add_html_file('options.html')
@@ -23,7 +20,7 @@ def build(source_dir, base_build_path=None):
   build_dir = app_builder.build()
   build_archive = util.create_archive(build_dir)
   return build_dir, build_archive
-  
+
 
 if __name__ == '__main__':
   builder.run(build)
