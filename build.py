bd370392abcdc26ab43bab2e6841e0e7bcfb53d3
Oleg Eterevsky <oleg@chromium.org>
2014-01-31 21:55:54 +0400

    Add command-line options to build.py.

--- build.py
+++ build.py
@@ -7,10 +7,11 @@ import builder.util as util
 FILES = ['main.css', 'options.css']
 
 @builder.action
-def build(source_dir, base_build_path=None):
+def dist(source_dir : builder.File, base_build_path, debug=False):
+  """Create a distribution in a directory and an archive."""
   _, short_name = chrome_app.get_name_from_manifest(source_dir)
-  build_path = util.build_path_with_version(source_dir, base_build_path,
-                                            short_name)
+  build_path = util.get_build_path_with_version(source_dir, base_build_path,
+                                                short_name)
 
   app_builder = chrome_app.Builder(source_dir, build_path)
   app_builder.add_static_files(FILES)
@@ -21,4 +22,4 @@ def build(source_dir, base_build_path=None):
 
 
 if __name__ == '__main__':
-  builder.run(build)
+  builder.run(dist)
