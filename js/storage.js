45b4ebe460938df7295d3a90ab1f6046fd199072
Oleg Eterevsky <oleg@eterevsky.com>
2014-02-12 19:20:50 +0400

    Implement getStorage().

--- js/storage.js
+++ js/storage.js
@@ -10,10 +10,68 @@
  *   'local'     - chrome.storage.local, localStorage
  *   'session'   - sessionStorage
  *   'memory'    - in-memory array
+ *
+ * At most one instance of storage for each type is maintained. The exposed
+ * interface of the storage is identical to the API of chrome.storage.
  */
+
+var storageByType = [];
 
 /**
  * @param {string} type
  */
 function getStorage(type) {
+  if (type in storageByType) {
+    return storageByType[type];
+  }
+
+  storage = null;
+
+  switch (type) {
+    case 'permanent':
+      if (chrome && chrome.storage) {
+        storage = chrome.storage.sync;
+      } else {
+        storage = new WebStorageWrapper(localStorage);
+      }
+      break;
+
+    case 'local':
+      if (chrome && chrome.storage) {
+        storage = chrome.storage.local;
+      } else {
+        storage = new WebStorageWrapper(localStorage);
+      }
+      break;
+
+    case 'session':
+      storage = new WebStorageWrapper(sessionStorage);
+      break;
+
+    case 'memory':
+      storage = new ObjectStorage();
+      break;
+
+    default:
+      throw "Unknown storage type: " + type;
+  }
+
+  storageByType[type] = storage;
+  return storage;
 }
+
+
+/**
+ * @constructor
+ * @implements {Storage}
+ */
+function ObjectStorage() {
+  this.values_ = {};
+}
+
+ObjectStorage.prototype.get = function(keys, callback) {
+  return this.values_[key];
+};
+
+ObjectStorage.prototype.set = function(key, value) {
+};
